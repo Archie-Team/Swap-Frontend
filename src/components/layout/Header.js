c1137f8904fb0440af8de7a0b3fa87d1ecc6eed7
htm_masoomi <htm_masoomi@yahoo.com>
2022-07-23 18:04:18 +0430

    responsive navbar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,5 @@
-import React from "react";
-import BULC from '../../assets/images/BULKLogo.svg'
+import React, { useState } from "react";
+import BULC from "../../assets/images/BULKLogo.svg";
 
 import { navigationItems } from "../../modules/varibales";
 import { NavLink } from "react-router-dom";
@@ -7,28 +7,48 @@ import "./Header.css";
 import ConnectWallet from "./ConnectWallet";
 
 const Header = () => {
+  const [navbarActivate, setNavbarActivate] = useState("");
+
+  const toggleNavbarHandler = () => {
+    setNavbarActivate(navbarActivate === "active" ? "" : "active");
+
+  };
 
 
   return (
     <header className="header">
-      <div className="bullcoin-logo">
-        <img src={BULC} alt="" />
-        <p>Bullcoin</p>
-      </div>
-      <nav className="nav">
-        <ul>
+      <nav className="nav navbar">
+        <div className="nav-logo">
+          <img src={BULC} alt="" />
+          <p>Bullcoin</p>
+        </div>
+        <ul className={"nav-menu " + navbarActivate}>
           {navigationItems.map((item) => (
-            <li key={item.link}>
-              <NavLink to={item.link} activeClassName="active" exact>
+            <li className="nav-item" key={item.link}>
+              <NavLink
+                className="nav-link"
+                to={item.link}
+                activeClassName="active"
+                exact
+              >
                 {item.name}
               </NavLink>
             </li>
           ))}
         </ul>
+        <div className="navbar__connect-wallet">
+          <ConnectWallet />
+        </div>
+        <div
+          className={"hamburger " + navbarActivate}
+          onClick={toggleNavbarHandler}
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+        
       </nav>
-
-        <ConnectWallet />
-
     </header>
   );
 };
