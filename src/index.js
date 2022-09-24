0c6406d225a9240c52f361bff3e3bd1fd40d97e6
htm_masoomi <htm_masoomi@yahoo.com>
2022-09-24 11:39:32 +0330

    add all jsons

--- src/index.js
+++ src/index.js
@@ -3,25 +3,11 @@ import ReactDOM from "react-dom/client";
 // import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-<<<<<<< HEAD
-import store from "./store/index";
-import { Provider } from "react-redux";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-=======
 import { HashRouter } from "react-router-dom";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
-      <App />
+    <App />
   </HashRouter>
->>>>>>> 503f2d25845383952fc3dd2ace465a49daa24ed6
 );
