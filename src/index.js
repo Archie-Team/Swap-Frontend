536b26abf94ca201ac0b913c7b3c2e389ddf5297
htm_masoomi <htm_masoomi@yahoo.com>
2022-09-24 11:41:31 +0330

    return provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import store from "./store/index";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
